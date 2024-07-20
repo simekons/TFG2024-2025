b8bfa74b6c67c2e008ca715aeaccc289567cfdd6
simekons <simekons@ucm.es>
2024-07-20 18:25:12 +0200

    Cambios en el juego MEMORY
    Ahora vuelve a iniciar la secuencia si se ha introducido incorrectamente.

--- Unity/Demo/Assets/Scripts/Memory.cs
+++ Unity/Demo/Assets/Scripts/Memory.cs
@@ -15,87 +15,103 @@ public class Memory : MonoBehaviour
 
     public static Memory instance = null;
 
+    /// <summary>
+    /// Instance the Memory manager.
+    /// </summary>
     private void Awake()
     {
-        if (instance != null)
-        {
+        if (instance != null) 
             Debug.LogError("Found more than one AudioManager in scene");
-        }
+        
         instance = this;
     }
 
-    // Start is called before the first frame update
+    /// <summary>
+    /// Initialize the random sequence.
+    /// </summary>
     void Start()
     {
         randomNumbers = new List<int>();
-        for (int i = 0; i < count; i++)
-        {
+        for (int i = 0; i < count; i++) {
             int randomNumber = Random.Range(0, 5);
             randomNumbers.Add(randomNumber);
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (!recorded)
-        {
+    /// <summary>
+    /// Keeps track of the current sequence input.
+    /// </summary>
+    void Update() {
+        if (!recorded) {
             buttonsInteract(false);
             Invoke("buttonsInteractT", countMem);
-            if (countMem <= count)
-            {
-                for (int i = 0; i < countMem; i++)
-                {
+
+            if (countMem <= count) {
+                for (int i = 0; i < countMem; i++) {
                     buttons[randomNumbers[i]].GetComponent<PlaySound>().playInTime(i);
                 }
                 recorded = true;
             }
-            else
-            {
+            else {
                 buttonsInteract(false);
                 panel.SetActive(true);
             }
         }
     }
 
+    /// <summary>
+    /// Check if the sequence input is correct.
+    /// </summary>
     public void takeCombination(int num)
     {
         if (randomNumbers[correctButton] == num)
-        {
             correctButton++;
-        }
-        else
-        {
+        else {
             correctButton = 0;
+            Invoke("restart", 1.0f);
         }
-        if (correctButton == countMem)
-        {
+        if (correctButton == countMem) {
             Invoke("nextCode", 0.8f);
             correctButton = 0;
         }
     }
 
-    private void nextCode()
+    private void restart()
     {
         recorded = false;
+    }
+
+    /// <summary>
+    /// Enlarges the sequence by one.
+    /// </summary>
+    private void nextCode()
+    {
+        restart();
         countMem++;
     }
 
+    /// <summary>
+    /// Button interaction.
+    /// </summary>
     private void buttonsInteractT()
     {
         buttonsInteract(true);
     }
 
+    /// <summary>
+    /// Button interacion.
+    /// </summary>
     private void buttonsInteract(bool interact)
     {
         for (int i = 0; i < buttons.Length; i++)
-        {
             buttons[i].GetComponent<Button>().interactable = interact;
-        }
     }
 
+    /// <summary>
+    /// Starts the game.
+    /// </summary>
     public void startGame()
     {
-        recorded = false;
+        restart();
     }
 }
